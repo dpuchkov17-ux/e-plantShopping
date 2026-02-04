6cdd48f8e226f55e009051dc487c72cbd4046ddb
Dmitry Puchkov <dpuchkov@gmail.com>
2026-02-04 14:27:03 -0500

    Undelining removed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -269,12 +269,17 @@ const handleAddToCart = (product, groupCategory) => {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/" onClick={(e) => handleHomeClick(e)}>
-                            <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
-                            </div>
-                        </a>
+                        <a
+  href="/"
+  className="navbar-logo-link"
+  onClick={(e) => handleHomeClick(e)}
+>
+  <div>
+    <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
+    <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+  </div>
+</a>
+
                     </div>
 
                 </div>
